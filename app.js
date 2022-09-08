8f7a572da4cdc071f3a09eacb22d37452c76dcfd
chengyen.chiang <chengyen.chiang@leediancn.com>
2022-09-08 22:28:07 +0800

    reserveHouse init

--- app.js
+++ app.js
@@ -5,11 +5,13 @@ const serverPort = config.serverPort;
 const server = require('http').createServer(app);
 const serverUse=require('./lib/serverUse');
 const houseRestApi = require("./lib/rest_api/house");
+const reserveHouseRestApi = require("./lib/rest_api/reserveHouse");
 
 console.log('config',config)
 
 serverUse.on(app);
 houseRestApi.on(app);
+reserveHouseRestApi.on(app);
 
 server.listen(serverPort);
 console.log("現在使用" + serverPort + " port");
