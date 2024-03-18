7834036420bd9b640853ce3f73d5e289e330a5b0
chengyen.chiang <chengyen.chiang@leediancn.com>
2024-03-18 23:44:02 +0800

    add swag

--- app.js
+++ app.js
@@ -6,12 +6,14 @@ const server = require('http').createServer(app);
 const serverUse=require('./lib/serverUse');
 const houseRestApi = require("./lib/rest_api/house");
 const reserveHouseRestApi = require("./lib/rest_api/reserveHouse");
+const houseDevRestApi = require("./lib/rest_api/houseDev");
 
 console.log('config',config)
 
 serverUse.on(app);
 houseRestApi.on(app);
 reserveHouseRestApi.on(app);
+houseDevRestApi.on(app);
 
 server.listen(serverPort);
 console.log("現在使用" + serverPort + " port");
